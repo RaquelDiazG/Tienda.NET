d86b050578f88ed34619f9b61432e41dbcb3fbc4
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2016-03-16 12:18:27 +0100

    Añadido servicio web (tiempo en madrid) en la pagina de contacto

--- TiendaOnline/TiendaOnline/Controllers/IndexController.cs
+++ TiendaOnline/TiendaOnline/Controllers/IndexController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 
 namespace TiendaOnline.Controllers
 {
@@ -16,6 +17,17 @@ namespace TiendaOnline.Controllers
 
         public ActionResult Contacto()
         {
+            ServiceReference1.GlobalWeatherSoapClient client = new ServiceReference1.GlobalWeatherSoapClient("GlobalWeatherSoap");
+            string result = client.GetWeather("Madrid", "Spain");
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(result);
+            XmlNodeList xnList = xml.SelectNodes("/CurrentWeather");
+            string temperatura = "";
+            foreach (XmlNode xn in xnList)
+            {
+                temperatura = xn["Temperature"].InnerText;
+            }
+            ViewBag.Temperatura = temperatura;
             return View();
         }
     }
