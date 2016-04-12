3f7e042dbfa83f1b83afe6127d9e877191704cc8
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2016-04-12 15:17:34 +0200

    Corregida excepcion con servicio web

--- TiendaOnline/TiendaOnline/Controllers/IndexController.cs
+++ TiendaOnline/TiendaOnline/Controllers/IndexController.cs
@@ -17,15 +17,23 @@ namespace TiendaOnline.Controllers
 
         public ActionResult Contacto()
         {
-            ServiceReference1.GlobalWeatherSoapClient client = new ServiceReference1.GlobalWeatherSoapClient("GlobalWeatherSoap");
-            string result = client.GetWeather("Madrid", "Spain");
-            XmlDocument xml = new XmlDocument();
-            xml.LoadXml(result);
-            XmlNodeList xnList = xml.SelectNodes("/CurrentWeather");
             string temperatura = "";
-            foreach (XmlNode xn in xnList)
+            try
             {
-                temperatura = xn["Temperature"].InnerText;
+                ServiceReference1.GlobalWeatherSoapClient client = new ServiceReference1.GlobalWeatherSoapClient("GlobalWeatherSoap");
+                string result = client.GetWeather("Madrid", "Spain");
+                XmlDocument xml = new XmlDocument();
+                xml.LoadXml(result);
+                XmlNodeList xnList = xml.SelectNodes("/CurrentWeather");
+                
+                foreach (XmlNode xn in xnList)
+                {
+                    temperatura = xn["Temperature"].InnerText;
+                }
+            }
+            catch (Exception ex)
+            {
+                temperatura = "No disponible";
             }
             ViewBag.Temperatura = temperatura;
             return View();
